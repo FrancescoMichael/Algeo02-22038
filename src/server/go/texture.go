d9f3d25ca6c3afad8f2db8da753c1578c906910d
Julian <julianchandrasutadi@gmail.com>
2023-11-12 19:02:53 +0700

    feat: migrate texture-based CBIR from Python to Go

---
+++ src/server/go/texture.go
@@ -0,0 +1,265 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/draw"
+	"image/jpeg"
+	"image/png"
+	"math"
+	"os"
+	"path/filepath"
+	"sort"
+	"time"
+)
+
+func main() {
+	startTime := time.Now()
+
+	type Tuple struct {
+		idx        int
+		similarity float64
+	}
+
+	testFeature := createFeatureVector(createSymmetricMatrix(createCoocurenceMatrix(rgbToGreyscale(imgToRGB("948.jpg")))))
+
+	dirList := listDir("../../../test/")
+	rgbMatrices := loadImagesFromDir("../../../test/", dirList)
+
+	var similarityList []Tuple
+
+	length := len(rgbMatrices)
+	for i := 0; i < length; i++ {
+		feature := createFeatureVector(createSymmetricMatrix(createCoocurenceMatrix(rgbToGreyscale(imgToRGB(dirList[i])))))
+		var temp Tuple
+		temp.idx = i
+		temp.similarity = cosineSimilarity(testFeature, feature)
+		similarityList = append(similarityList, temp)
+	}
+
+	sort.Slice(similarityList, func(i, j int) bool {
+		return similarityList[i].similarity > similarityList[j].similarity
+	})
+
+	fmt.Println("5 foto terdekat pertama:")
+	for i := 0; i < 5; i++ {
+		fmt.Println(dirList[similarityList[i].idx])
+	}
+
+	endTime := time.Now()
+	elapsed := endTime.Sub(startTime)
+
+	fmt.Printf("Program execution time: %v\n", elapsed)
+}
+
+func imgToRGB(filename string) [][][3]uint8 {
+	// file, _ := os.Open(filename)
+
+	// var img image.Image
+	// format := filepath.Ext(filename)
+	// switch format {
+	// case ".jpg", ".jpeg":
+	// 	img, _ = jpeg.Decode(file)
+	// case ".png":
+	// 	img, _ = png.Decode(file)
+	// }
+	name := "../../../test/" + filename
+
+	file, err := os.Open(name)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	var img image.Image
+	format := filepath.Ext(name)
+	switch format {
+	case ".jpg", ".jpeg":
+		img, err = jpeg.Decode(file)
+	case ".png":
+		img, err = png.Decode(file)
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// referensi: https://go.dev/blog/image-draw#TOC_6.
+	b := img.Bounds()
+	rgba := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
+	draw.Draw(rgba, rgba.Bounds(), img, b.Min, draw.Src)
+
+	rgb := sliceRGB(*rgba)
+
+	return rgb
+}
+
+func sliceRGB(rgba image.RGBA) [][][3]uint8 {
+	width, height := rgba.Bounds().Dx(), rgba.Bounds().Dy()
+	rgb := make([][][3]uint8, height)
+
+	for i := 0; i < height; i++ {
+		rgb[i] = make([][3]uint8, width)
+		for j := 0; j < width; j++ {
+			r, g, b, _ := rgba.At(j, i).RGBA()
+			rgb[i][j][0] = uint8(r >> 8)
+			rgb[i][j][1] = uint8(g >> 8)
+			rgb[i][j][2] = uint8(b >> 8)
+		}
+	}
+
+	return rgb
+}
+
+func rgbToGreyscale(rgb [][][3]uint8) [][]uint8 {
+	row := len(rgb)
+	col := len(rgb[0])
+
+	greyscale := make([][]uint8, row)
+
+	for i, x := range rgb {
+		greyscale[i] = make([]uint8, col)
+		for j, val := range x {
+			greyscale[i][j] = uint8(float64(val[0])*0.299 + float64(val[1])*0.587 + float64(val[2])*0.114)
+		}
+	}
+
+	return greyscale
+}
+
+func createCoocurenceMatrix(greyscale [][]uint8) [256][256]uint {
+	coocurence := [256][256]uint{}
+
+	row := len(greyscale)
+	col := len(greyscale[0])
+
+	for i := 0; i < row; i++ {
+		for j := 0; j < col-1; j++ {
+			coocurence[greyscale[i][j]][greyscale[i][j+1]] += 1
+		}
+	}
+	return coocurence
+}
+
+func createSymmetricMatrix(coocurence [256][256]uint) [256][256]float64 {
+	transposedCoocurence := transpose(coocurence)
+	symmetric := [256][256]float64{}
+	for i := 0; i < 256; i++ {
+		for j := 0; j < 256; j++ {
+			symmetric[i][j] += float64(coocurence[i][j] + transposedCoocurence[i][j])
+		}
+	}
+
+	var total float64
+	for i := 0; i < 256; i++ {
+		for j := 0; j < 256; j++ {
+			total += symmetric[i][j]
+		}
+	}
+
+	// normalization
+	for i := 0; i < 256; i++ {
+		for j := 0; j < 256; j++ {
+			symmetric[i][j] /= total
+		}
+	}
+
+	return symmetric
+}
+
+func createFeatureVector(symmetric [256][256]float64) [5]float64 {
+	contrast := 0.
+	homogeneity := 0.
+	asm := 0.
+	energy := 0.
+	entropy := 0.
+	sumVal := 0.
+	glcmMean := 0.
+	correlation := 0.
+	glcmVariance := 0.
+
+	var value float64
+
+	for i := 0; i < 256; i++ {
+		for j := 0; j < 256; j++ {
+			value = symmetric[i][j]
+			contrast += value * math.Pow(float64(i-j), 2)
+			homogeneity += value / (1 + math.Pow(float64(i-j), 2))
+			asm += math.Pow(value, 2)
+			sumVal += value
+			glcmMean += float64(i) * value
+			if value != 0 {
+				entropy -= value * math.Log(value)
+			}
+		}
+	}
+
+	for i := 0; i < 256; i++ {
+		for j := 0; j < 256; j++ {
+			glcmVariance += symmetric[i][j] * math.Pow(float64(i)-glcmMean, 2)
+		}
+	}
+
+	for i := 0; i < 256; i++ {
+		for j := 0; j < 256; j++ {
+			correlation += symmetric[i][j] * (float64(i) - glcmMean) * (float64(j) - glcmMean) / glcmVariance
+		}
+	}
+
+	energy = math.Sqrt(asm)
+	return [5]float64{contrast, homogeneity, entropy, energy, correlation}
+}
+
+func transpose(matrix [256][256]uint) [256][256]uint {
+	transposedMatrix := [256][256]uint{}
+
+	for i := 0; i < 256; i++ {
+		for j := 0; j < 256; j++ {
+			transposedMatrix[j][i] = matrix[i][j]
+		}
+	}
+
+	return transposedMatrix
+}
+
+func cosineSimilarity(featureVector1, featureVector2 [5]float64) float64 {
+	result := 0.
+	// dot product
+	for i := 0; i < 5; i++ {
+		result += featureVector1[i] * featureVector2[i]
+	}
+
+	// norm
+	norm1 := 0.
+	norm2 := 0.
+
+	for i := 0; i < 5; i++ {
+		norm1 += math.Pow(featureVector1[i], 2)
+		norm2 += math.Pow(featureVector2[i], 2)
+	}
+
+	norm1 = math.Sqrt(norm1)
+	norm2 = math.Sqrt(norm2)
+
+	result /= (norm1 * norm2)
+	return result
+}
+
+func listDir(path string) []string {
+	dir, _ := os.Open(path)
+	fileInfos, _ := dir.Readdir(-1)
+
+	var dirList []string
+	for _, fileInfo := range fileInfos {
+		dirList = append(dirList, fileInfo.Name())
+	}
+
+	return dirList
+}
+
+func loadImagesFromDir(path string, dirList []string) [][][][3]uint8 {
+	var rgbMatrices [][][][3]uint8
+	for _, x := range dirList {
+		rgbMatrices = append(rgbMatrices, imgToRGB(x))
+	}
+	return rgbMatrices
+}
