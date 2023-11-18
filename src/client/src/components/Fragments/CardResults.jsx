fe52a214f1d8222694ca408749dd78eb8bc82351
FrancescoMichael <francescokusuma15@gmail.com>
2023-11-18 15:52:52 +0700

    pagination

--- src/client/src/components/Fragments/CardResults.jsx
+++ src/client/src/components/Fragments/CardResults.jsx
@@ -1,58 +1,41 @@
 import React from "react";
 
-import "./CardResults.css";
+function CardResults(props){
+    const { children } = props;
 
-const CardResults = ({ image, percentage }) => {
     return (
-        <div className='card'>
-            <div className='card_image'>
-                <img src={image} />
-            </div>
-            <div className='card_info'>
-                <h3>{percentage}</h3>
-            </div>
+        <div className = " max-w-xs bg-g-800 border border-gray-700 rounded-lg shadow mx-3 my-2 flex flex-col justify-between">
+            {children}
         </div>
     );
-};
+}
 
-export default CardResults;
+function Gambar(props){
+    const { image } = props;
 
-// function CardResults(props){
-//     const { children } = props;
+    return (
+        <a href = "#">
+            <img 
+            src = {image} 
+            alt = "imageresult" 
+            className = "p-5 rounded-t-lg h-60 w-full object-cover"></img>
+        </a>
+    );
+}
 
-//     return (
-//         <div className = "w-full max-w-xs bg-g-800 border border-gray-700 rounded-lg shadow mx-3 my-2 flex flex-col justify-between">
-//             {children}
-//         </div>
-//     );
-// }
+function Persentase(props){
+    const { percentage } = props;
 
-// function Gambar(props){
-//     const { image } = props;
+    return (
+        <div className = "flex items-center justify-center px-5 pb-5">
+            <span className = "text-xl font-bold text-black">
+                {percentage}
+            </span>
+        </div>
+    );
+}
 
-//     return (
-//         <a href = "#">
-//             <img 
-//             src = {image} 
-//             alt = "imageresult" 
-//             className = "p-8 rounded-t-lg h-60 w-full object-cover"></img>
-//         </a>
-//     );
-// }
+CardResults.Gambar = Gambar;
+CardResults.Persentase = Persentase;
 
-// function Persentase(props){
-//     const { percentage } = props;
-
-//     return (
-//         <div className = "flex items-center justify-center px-5 pb-5">
-//             <span className = "text-xl font-bold text-white">
-//                 {percentage}
-//             </span>
-//         </div>
-//     );
-// }
-
-// CardResults.Gambar = Gambar;
-// CardResults.Persentase = Persentase;
-
-// export default CardResults
+export default CardResults
--- src/client/src/services/data.services.js
+++ src/client/src/services/data.services.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getData = () => {
     axios
-    .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+    .get("")
     .then((res) => {
         console.log(res.data)
         // callback(res.data)
