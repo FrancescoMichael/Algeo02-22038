c9e2bf8a9f7b41406ab2597b004daf4b0bb0c92c
Julian <julianchandrasutadi@gmail.com>
2023-11-18 18:53:33 +0700

    feat: execution time endpoint

--- src/server/go/routes.go
+++ src/server/go/routes.go
@@ -2,23 +2,38 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+var executionTime time.Duration
+var startTime time.Time
+var endTime time.Time
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/images", getSimilarImgs)
 	router.GET("/imagescache", getSimilarImgsCache)
+	router.GET("/executiontime", getExecutionTime)
 
 	router.Run("localhost:8080")
 }
 
 func getSimilarImgs(c *gin.Context) {
+	startTime = time.Now()
 	c.IndentedJSON(http.StatusCreated, texture())
+	executionTime = time.Since(startTime)
 }
 
 func getSimilarImgsCache(c *gin.Context) {
+	startTime = time.Now()
 	c.IndentedJSON(http.StatusCreated, textureWithCache())
+	executionTime = time.Since(startTime)
+}
+
+func getExecutionTime(c *gin.Context) {
+	c.IndentedJSON(http.StatusCreated, gin.H{"id": 1, "executiontime": executionTime.String()})
+	executionTime = 0
 }
