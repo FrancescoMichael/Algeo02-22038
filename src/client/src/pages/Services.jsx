af2eba312a42a05e7ad934ed8e6ff14c7f762b9d
Julian <julianchandrasutadi@gmail.com>
2023-11-19 15:07:33 +0700

    feat: added styling to web

--- src/client/src/pages/Services.jsx
+++ src/client/src/pages/Services.jsx
@@ -2,115 +2,65 @@
 import React from "react";
 
 function Services () {
-    let message = `There are many variations of passages of Lorem Ipsum available but the \n majority have suffered alteration in some injected humour.`;
     return (
-      <section class="section-white">
+    <section class="section-background">
  
-    <div class="container">
- 
-        <div class="row">
-       
-                    <div class="col-md-12 text-center">
+      <div class="container">
+  
+            <div class="head">
+                      <div class="col-md-12 text-center">
+                            <h2 class="section-title">The Team Behind <em>Jaring</em></h2>
 
-                          <h2 class="section-title">The Team Behind Pacifico</h2>
+                      </div> 
+            <div class="profiles">
+                <div class="team-item">
+                
+                    <img src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team1a.jpg" class="team-img" alt="pic" />                   
+                    <h3>FRANCESCO MICHAEL KUSUMA</h3>            
+                    <div class="team-nim"><p>13522038</p></div>
+                
+                    <ul class="team-icon">
+                    
+                        <li><a href="https://github.com/FrancescoMichael" class="github"><img src="src/assets/github.png"></img></a></li>
+                        
+                    </ul>
+                    
+                
+                </div> 
 
-                          <p class="section-subtitle">{message}</p>
-                          
-                    </div> 
-             
-            <div class="col-sm-6 col-md-4">
+                <div class="team-item">
+                
+                    <img src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team2a.jpg" class="team-img" alt="pic" />
+                    
+                    <h3>MARVEL PANGONDIAN</h3>
+                    
+                    <div class="team-nim"><p>13522075</p></div>
 
-                  <div class="team-item">
-                  
-                      <img src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team1a.jpg" class="team-img" alt="pic" />                   
-                      <h3>JOHNATHAN HAWKINS</h3>            
-                      <div class="team-info"><p>Head of SEO</p></div>
-                      <p>Johnathan is our  co-founder and has developed search strategies for a variety of clients from international brands to medium sized businesses for over five years.</p>
-                  
-                      <ul class="team-icon">
-                      
-                          <li><a href="#" class="twitter">
-                            <i class="fa fa-twitter"></i>
-                        </a></li>
-                          
-                          <li><a href="#" class="pinterest">
-                            <i class="fa fa-pinterest"></i>
-                        </a></li>
-                          
-                          <li><a href="#" class="facebook">
-                            <i class="fa fa-facebook"></i>
-                        </a></li>
-                          
-                          <li><a href="#" class="dribble">
-                            <i class="fa fa-dribbble"></i>
-                        </a></li>
-                          
-                      </ul>
-                      
-                  
+                    <ul class="team-icon">
+                    
+                        <li><a href="https://github.com/MarvelPangondian" class="github"><img src="src/assets/github.png"></img></a></li>
+                        
+                    </ul>
+                    
                 </div>
-            </div> 
-              
-            <div class="col-sm-6 col-md-4">
 
-                  <div class="team-item">
-                  
-                      <img src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team2a.jpg" class="team-img" alt="pic" />
-                     
-                      <h3>ALEXANDRA SMITHS</h3>
-                      
-                      <div class="team-info"><p>SEO Specialist</p></div>
+                <div class="team-item">
+                
+                    <img src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team3a.jpg" class="team-img" alt="pic" />
+                    
+                    <h3>JULIAN CHANDRA SUTADI</h3>
+                    <div class="team-nim"><p>1352080</p></div>
+                    <ul class="team-icon">
+                        <li><a href="https://github.com/julianchandras" class="github"><img src="src/assets/github.png" alt=""></img></a></li>
+                    </ul>
+                    
+                </div>
 
-                      <p>Graduating with a degree in Spanish and English, Alexandra has always loved writing and now she’s lucky enough to do it as part of her new job inside our agency.</p>
-                  
-                      <ul class="team-icon">
-                      
-                          <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
-                          
-                          <li><a href="#" class="pinterest"><i class="fa fa-pinterest"></i></a></li>
-                          
-                          <li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
-                          
-                          <li><a href="#" class="dribble"><i class="fa fa-dribbble"></i></a></li>
-                          
-                      </ul>
-                      
-                  </div>
-
-            </div> 
-            <div class="col-sm-6 col-md-4">
-
-                  <div class="team-item">
-                  
-                      <img src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team3a.jpg" class="team-img" alt="pic" />
-                     
-                      <h3>ELISA JOHANSON</h3>
-                      
-                      <div class="team-info"><p>Marketing Manager</p></div>
-
-                      <p>Elisa first fell in love with digital marketing at the university. He loves to learn, and looks forward to being part of this new exciting industry for many years.</p>
-                  
-                      <ul class="team-icon">
-                      
-                          <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
-                          
-                          <li><a href="#" class="pinterest"><i class="fa fa-pinterest"></i></a></li>
-                          
-                          <li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
-                          
-                          <li><a href="#" class="dribble">
-                            <i class="fa fa-dribbble"></i>
-                        </a></li>
-                          
-                      </ul>
-                      
-                  </div>
-
-            </div> 
-        
-        </div> 
-    
-    </div> 
+            </div>
+          
+          </div> 
+      
+      </div> 
 
     </section>
     )
