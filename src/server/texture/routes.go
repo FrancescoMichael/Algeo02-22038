15e74bf94ec6fb24c3fb85953e9ccb568beb5cc3
Julian <julianchandrasutadi@gmail.com>
2023-11-19 15:46:08 +0700

    feat: allowed access to other url

--- src/server/texture/routes.go
+++ src/server/texture/routes.go
@@ -14,6 +14,13 @@ var endTime time.Time
 func main() {
 	router := gin.Default()
 
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // Replace with the origin of your frontend
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		c.Next()
+	})
+
 	router.GET("/images", getSimilarImgs)
 	router.GET("/imagescache", getSimilarImgsCache)
 	router.GET("/executiontime", getExecutionTime)
